49e53699acb90aacee946811ddc0bdd27b5ae87a
Anica Sutic <anica.sutic@gmail.com>
2020-04-23 18:46:37 +0200

    Added Loading Screens

---
+++ components/Loading.tsx
@@ -0,0 +1,51 @@
+import React, { useEffect } from "react"
+import { StyleSheet, View, Text } from "react-native"
+import AnimatedLoader from "react-native-animated-loader";
+
+const Loading = (props: any) => {
+
+    useEffect(() => {
+        setTimeout(function () {
+            props.setLoading(false)
+        }, 5000)
+    }, [])
+
+    return (
+        <View style={styles.loading}>
+            <AnimatedLoader
+                visible={true}
+                overlayColor="rgba(255,255,255,0.75)"
+                source={require("../assets/loader.json")}
+                animationStyle={styles.lottie}
+                speed={1}
+            />
+            {/* <AnimatedLoader
+                visible={true}
+                overlayColor="rgba(255,255,255,0.75)"
+                source={require("../assets/success.json")}
+                animationStyle={styles.lottie}
+                speed={1}
+            /> */}
+            <Text style={styles.loadingText}>Sending Data...</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    lottie: {
+        width: 100,
+        height: 100
+    },
+    loading: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    loadingText: {
+        fontSize: 20,
+        color: '#0E6EB8',
+        fontWeight: 'bold'
+    }
+});
+
+export default Loading;
--- components/SubmitPositive.tsx
+++ components/SubmitPositive.tsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from "react"
 import { Text, View, StyleSheet, Dimensions, TouchableOpacity } from "react-native"
 import { getUserData } from "../services/userDataStorageService"
+import Loading from "./Loading"
 
 
 const { width: WIDTH } = Dimensions.get('window')
 
 const SubmitPositive = (props: any) => {
     const [healthId, setHealthId] = useState("")
+    const [loading, setLoading] = useState(false)
 
     useEffect(() => {
         getUserData().then(
@@ -18,33 +20,36 @@ const SubmitPositive = (props: any) => {
 
     const submitPositiveResults = () => {
         console.log("SEND REQUEST")
+        setLoading(true)
     }
 
+
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <View style={{ marginBottom: 100, alignItems: 'center', justifyContent: 'center' }}>
-                <Text style={styles.label}>HEALTH ID</Text>
-                <Text style={styles.healthId}>{healthId}</Text>
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                {loading ? <Loading setLoading={setLoading} /> : [
+                <View style={{ flex: 2, alignItems: 'center', justifyContent: 'center' }}>
+                    <Text style={styles.label}>HEALTH ID</Text>
+                    <Text style={styles.healthId}>{healthId}</Text>
+                </View>,
+                <View style={{ flex: 1, marginBottom: 100, alignItems: 'center', justifyContent: 'center' }}>
+                    <TouchableOpacity onPress={() => submitPositiveResults()}>
+                        <View style={styles.buttonSave}>
+                            <Text style={{ fontSize: 20, color: 'white', fontWeight: 'bold' }}>SUBMIT</Text>
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => props.setSubmitMode(false)}>
+                        <View style={styles.buttonCancel}>
+                            <Text style={styles.btnCancelText}>CANCEL</Text>
+                        </View>
+                    </TouchableOpacity>
+                </View>]}
             </View>
-            <View>
-                <TouchableOpacity onPress={() => submitPositiveResults()}>
-                    <View style={styles.buttonSave}>
-                        <Text style={{ fontSize: 20, color: 'white', fontWeight: 'bold' }}>SUBMIT</Text>
-                    </View>
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => props.setSubmitMode(false)}>
-                    <View style={styles.buttonCancel}>
-                        <Text style={styles.btnCancelText}>CANCEL</Text>
-                    </View>
-                </TouchableOpacity>
-            </View>
-        </View>
     )
 }
 
 const styles = StyleSheet.create({
-    label: {
-        marginTop: 10,
+                    label: {
+                    marginTop: 10,
         marginBottom: 5,
         fontSize: 16,
         fontWeight: 'bold',
@@ -52,11 +57,11 @@ const styles = StyleSheet.create({
         textAlign: 'left'
     },
     healthId: {
-        fontSize: 40,
+                    fontSize: 40,
         fontStyle: 'italic'
     },
     buttonSave: {
-        backgroundColor: "#0E6EB8",
+                    backgroundColor: "#0E6EB8",
         borderWidth: 1,
         borderColor: '#0E6EB8',
         borderRadius: 50,
@@ -67,7 +72,7 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
     },
     buttonCancel: {
-        borderWidth: 1,
+                    borderWidth: 1,
         borderColor: '#0E6EB8',
         borderRadius: 50,
         width: WIDTH - 65,
@@ -77,7 +82,7 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
     },
     btnCancelText: {
-        fontSize: 20,
+                    fontSize: 20,
         color: '#0E6EB8',
         fontWeight: 'bold'
     }
